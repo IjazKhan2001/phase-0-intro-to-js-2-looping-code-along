23722724ca1f1c77a71b128a726d5284a0d748ff
Ijaz <khanijaz18@icloud.com>
2022-05-25 13:36:46 -0400

    commit tree 6819b7220a821a44c417e0a937ddc4cd02401ecf

--- index.js
+++ index.js
@@ -1,1 +1,12 @@
-// Code your solutions in this file
+const cards = ['Guadalupe', 'Ollie', 'Aki'];
+function writeCards(cards){
+  for(let i = 0; i < cards.length; i++){
+    console.log(`Thank you, ${cards[1]}, for the wonderful surprise gift`);
+  }
+
+  return cards
+}
+
+writeCards(cards[0]);
+writeCards(cards[1]);
+writeCards(cards[2]);
